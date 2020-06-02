834266a2e506b4d2dc82184fa70f7c20ebc4757c
Dhruv Shah <dhruvshah5784@gmail.com>
2020-06-01 20:18:56 -0400

    Initial Commit

---
+++ H4XOR News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4XOR News
+//
+//  Created by Dhruv Shah on 2020-06-01.
+//  Copyright © 2020 Dhruv Shah. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
